29399d65d50dae523a52dbaab7972e43dc7ac2f0
Muhammad Kalmani <muhammad.kalmani@student.manchester.ac.uk>
2025-03-18 14:23:06 +0000

    setup and added mapbox dependency to the pom.xml file. minor bug fixes

--- src/main/java/uk/ac/man/cs/eventlite/entities/Venue.java
+++ src/main/java/uk/ac/man/cs/eventlite/entities/Venue.java
@@ -7,6 +7,7 @@ import jakarta.persistence.*;
 public class Venue {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
     @Column(name = "name", nullable = false)
