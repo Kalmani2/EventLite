5abe56e3ca72b968dcccfad73515bc6247dd580d
Manan Malhotra <mananm@Manans-MacBook-Air-9.local>
2025-03-25 13:03:56 +0000

    Updated assembler to generate REST Events List data

--- src/main/java/uk/ac/man/cs/eventlite/assemblers/EventModelAssembler.java
+++ src/main/java/uk/ac/man/cs/eventlite/assemblers/EventModelAssembler.java
@@ -16,14 +16,11 @@ public class EventModelAssembler implements RepresentationModelAssembler<Event, 
     @Override
     public EntityModel<Event> toModel(Event event) {
         EntityModel<Event> eventModel = EntityModel.of(event,
-            linkTo(methodOn(EventsControllerApi.class).getEvent(event.getId())).withSelfRel(),
-            linkTo(methodOn(EventsControllerApi.class).getAllEvents()).withRel("events")
-        );
+                linkTo(methodOn(EventsControllerApi.class).getEvent(event.getId())).withSelfRel(),
+                linkTo(methodOn(EventsControllerApi.class).getAllEvents()).withRel("events"));
         if (event.getVenue() != null) {
-            eventModel.add(linkTo(EventsControllerApi.class)
-                    .slash(event.getId())
-                    .slash("venue")
-                    .withRel("venue"));
+            eventModel
+                    .add(linkTo(methodOn(EventsControllerApi.class).getVenueForEvent(event.getId())).withRel("venue"));
         }
         return eventModel;
     }
